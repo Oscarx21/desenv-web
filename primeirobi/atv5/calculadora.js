3f34a2db6b1d395308fe5cfc97d4b2aec2b1bcfc
Oscarx21 <oscarcoradi95@hotmail.com>
2026-04-01 16:45:22 -0300

    Atividade 5

---
+++ primeirobi/atv5/calculadora.js
@@ -0,0 +1,9 @@
+const valorConta = 350;
+
+const perentualGorjeta = 10;
+
+var valorGorjeta = (valorConta * perentualGorjeta) / 100;
+
+var valorTotal = valorConta + valorGorjeta;
+
+console.log("Valor da conta: R$ " + valorConta + ", Percentual da gorjeta: " + perentualGorjeta + "%, Valor da gorjeta: R$ " + valorGorjeta + ", Valor total: R$ " + valorTotal);
